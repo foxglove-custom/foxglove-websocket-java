7036770f37a1b5d046ccc5dd2f224cc7fa767972
lucumt <lucumt@gmai.com>
2023-11-02 10:47:02 +0800

    利用kafka播放时也能显示车辆信息

--- src/main/java/com/jiaruiblog/foxglove/thread/kafka/Send3DKafkaThread.java
+++ src/main/java/com/jiaruiblog/foxglove/thread/kafka/Send3DKafkaThread.java
@@ -2,9 +2,13 @@ package com.jiaruiblog.foxglove.thread.kafka;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jiaruiblog.foxglove.kafka.deserializer.SceneUpdateDeserializer;
+import com.jiaruiblog.foxglove.schema.ModelPrimitive;
+import com.jiaruiblog.foxglove.schema.SceneEntity;
 import com.jiaruiblog.foxglove.schema.SceneUpdate;
+import com.jiaruiblog.foxglove.schema.Timestamp;
 import com.jiaruiblog.foxglove.thread.SendDataThread;
 import com.jiaruiblog.foxglove.util.KafkaUtil;
+import com.jiaruiblog.foxglove.util.SceneUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -14,6 +18,7 @@ import org.yeauty.pojo.Session;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import static com.jiaruiblog.foxglove.util.DataUtil.getFormatedBytes;
@@ -29,9 +34,11 @@ public class Send3DKafkaThread extends SendDataThread {
     public void run() {
         Properties props = KafkaUtil.getConsumerProperties("group-1", SceneUpdateDeserializer.class.getName());
         String topic = "drive_3d";
+        List<ModelPrimitive> models = SceneUtil.addModels();
         try (KafkaConsumer<String, SceneUpdate> consumer = new KafkaConsumer<>(props)) {
             consumer.subscribe(Arrays.asList(topic));
             boolean validCode = StringUtils.isNotBlank(code);
+            int i = 0;
             while (running) {
                 ConsumerRecords<String, SceneUpdate> records = consumer.poll(Duration.ofSeconds(1));
                 for (ConsumerRecord<String, SceneUpdate> record : records) {
@@ -39,6 +46,16 @@ public class Send3DKafkaThread extends SendDataThread {
                     if (validCode) {
                         update.setChassisCode(code);
                     }
+                    // 减少车辆模型的显示频率
+                    if (i == 5) {
+                        i = 0;
+                        List<SceneEntity> entities = update.getEntities();
+                        Timestamp timestamp = entities.get(0).getTimestamp();
+                        SceneEntity carEntity = SceneUtil.createEntity("drive_car", "obstacle", "vehicle.car", timestamp);
+                        carEntity.setModels(models);
+                        entities.add(carEntity);
+                    }
+                    i++;
                     JSONObject jsonObject = (JSONObject) JSONObject.toJSON(update);
                     byte[] bytes = getFormatedBytes(jsonObject.toJSONString().getBytes(), index);
                     this.session.sendBinary(bytes);
--- src/main/java/com/jiaruiblog/foxglove/thread/test/Send3DThread.java
+++ src/main/java/com/jiaruiblog/foxglove/thread/test/Send3DThread.java
@@ -4,8 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.jiaruiblog.foxglove.schema.*;
 import com.jiaruiblog.foxglove.thread.SendDataThread;
-import com.jiaruiblog.foxglove.util.DataUtil;
-import com.jiaruiblog.foxglove.util.DateUtil;
+import com.jiaruiblog.foxglove.util.SceneUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.yeauty.pojo.Session;
 
@@ -14,7 +13,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,7 +28,7 @@ public class Send3DThread extends SendDataThread {
     public Send3DThread(int index, int frequency, Session session) {
         super(index, frequency, session);
         updateList = this.readEntityData();
-        models = this.addModels();
+        models = SceneUtil.addModels();
     }
 
     private List<SceneUpdate> readEntityData() {
@@ -54,8 +52,8 @@ public class Send3DThread extends SendDataThread {
                     oldTs = currentTs;
                 }
 
-                SceneEntity entity = this.createEntity(data[1], "obstacle", "vehicle.truck", null);
-                entity.setCubes(this.addCubes(data));
+                SceneEntity entity = SceneUtil.createEntity(data[1], "obstacle", "vehicle.truck", null);
+                entity.setCubes(SceneUtil.addCubes(data));
 
                 entityList.add(entity);
             }
@@ -66,22 +64,6 @@ public class Send3DThread extends SendDataThread {
         }
         log.info("==============解析完毕，共有" + updateList.size() + "条点云记录======================");
         return updateList;
-    }
-
-    private SceneEntity createEntity(String id, String frameId, String metaDataValue, Timestamp ts) {
-        if (ts == null) {
-            ts = DateUtil.createTimestamp();
-        }
-
-        List<KeyValuePair> metadata = Arrays.asList(new KeyValuePair("category", metaDataValue));
-
-        SceneEntity entity = new SceneEntity();
-        entity.setTimestamp(ts);
-        entity.setFrame_id(frameId);
-        entity.setFrame_locked(true);
-        entity.setId(id);
-        entity.setMetadata(metadata);
-        return entity;
     }
 
     @Override
@@ -110,7 +92,7 @@ public class Send3DThread extends SendDataThread {
 
             // 减少车辆模型的显示频率
             if (i % 5 == 0) {
-                SceneEntity carEntity = this.createEntity("drive_car", "obstacle", "vehicle.car", timestamp);
+                SceneEntity carEntity = SceneUtil.createEntity("drive_car", "obstacle", "vehicle.car", timestamp);
                 carEntity.setModels(models);
                 entities.add(carEntity);
             }
@@ -129,63 +111,5 @@ public class Send3DThread extends SendDataThread {
         }
     }
 
-    private List<ModelPrimitive> addModels() {
-        ModelPrimitive model = new ModelPrimitive();
-        Pose pose = new Pose();
-        Vector3 position = new Vector3(-11.200000f, -8.499990f, 1.623f);
-        Quaternion orientation = new Quaternion(0.0f, 0.0f, 0f, 0.1f);
-        pose.setPosition(position);
-        pose.setOrientation(orientation);
 
-        Vector3 scale = new Vector3(1f, 1f, 1f);
-        //Color color = new Color(0.6f, 0.2f, 1f, 0.8f);
-        Color color = new Color(1f, 0.38823529411764707f, 0.2784313725490196f, 0.5f);
-        byte[] bytes = DataUtil.loadGlbData("car.glb");
-
-        model.setColor(color);
-        model.setPose(pose);
-        model.setData(bytes);
-        model.setScale(scale);
-        model.setMedia_type("model/gltf-binary");
-
-        return Arrays.asList(model);
-    }
-
-    private List<CubePrimitive> addCubes(String[] data) {
-        CubePrimitive cube = new CubePrimitive();
-        Color color = this.setCubeColor(Integer.parseInt(data[7]));
-
-        float length = Float.parseFloat(data[4]);
-        float width = Float.parseFloat(data[5]);
-        float height = length < width ? length : width;
-        Vector3 size = new Vector3(length, width, height);
-
-        Pose pose = new Pose();
-        float x = Float.parseFloat(data[2]);
-        float y = Float.parseFloat(data[3]);
-        Vector3 position = new Vector3(x, y, 10f);
-        float w = Float.parseFloat(data[6]);
-        Quaternion orientation = new Quaternion(0f, 0f, 1f, w);
-        pose.setPosition(position);
-        pose.setOrientation(orientation);
-
-        cube.setColor(color);
-        cube.setSize(size);
-        cube.setPose(pose);
-        List<CubePrimitive> cubeList = Arrays.asList(cube);
-        return cubeList;
-    }
-
-    private Color setCubeColor(int type) {
-        switch (type) {
-            case 0:
-                return new Color(0.1843137254901961f, 0.30980392156862746f, 0.30980392156862746f, 0.5f);
-            case 1:
-                return new Color(0.1137254901960784f, 0.0784313725490196f, 0.1f, 0.58f);
-            case 2:
-                return new Color(0f, 0f, 0.9019607843137255f, 0.5f);
-            default:
-                return new Color(1f, 0.38823529411764707f, 0.2784313725490196f, 0.5f);
-        }
-    }
 }
