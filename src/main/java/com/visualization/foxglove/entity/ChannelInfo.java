789b9dcd7d5cea16655d128d097ecc076618f767
lucumt <lucumt@gmail.com>
2024-04-10 13:26:52 +0800

    添加相关注释

--- src/main/java/com/visualization/foxglove/entity/ChannelInfo.java
+++ src/main/java/com/visualization/foxglove/entity/ChannelInfo.java
@@ -12,16 +12,33 @@ import lombok.Data;
 @Data
 public class ChannelInfo {
 
+    /**
+     * topic id，在注册时指定其值
+     */
     private Integer id;
 
+    /**
+     * topic名称
+     */
     private String topic;
 
+    /**
+     * topic编码，如json,protobuf等
+     */
     private String encoding;
 
+    /**
+     * topic描述
+     */
     private String schemaName;
 
+    /**
+     * topic对应的数据结构，可以是自定义的，也可以是foxglove官方定义好的
+     */
     private String schema;
 
+    /**
+     * 对用结构的表示形式，如json时为jsonschema
+     */
     private String schemaEncoding;
-
 }
